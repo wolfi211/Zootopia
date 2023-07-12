8c564720ff563dd8113a93e368e5fd45771cdee7
Daniel Wolf <wolf.dani9@gmail.com>
2023-07-12 17:01:08 +0200

    implemented basic CRUD for animals and enclosures

--- src/main/kotlin/eu/udemx/zootopia/controllers/ZootopiaRestController.kt
+++ src/main/kotlin/eu/udemx/zootopia/controllers/ZootopiaRestController.kt
@@ -1,8 +1,53 @@
 package eu.udemx.zootopia.controllers
 
+import eu.udemx.zootopia.models.entities.AnimalEntity
+import eu.udemx.zootopia.models.entities.EnclosureEntity
+import eu.udemx.zootopia.services.ZootopiaService
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class ZootopiaRestController {
+@RequestMapping("/api/v1")
+class ZootopiaRestController (
+    val zootopiaService: ZootopiaService
+) {
 
+    @GetMapping("/animals")
+    fun getAllAnimals() = zootopiaService.getAllAnimals()
+
+    @GetMapping("/animals/{id}")
+    fun getAnimalById(@PathVariable id: Long) = zootopiaService.getAnimalById(id)
+
+    @GetMapping("/enclosures")
+    fun getAllEnclosures() = zootopiaService.getAllEnclosures()
+
+    @GetMapping("/enclosures/{id}")
+    fun getEnclosureById(@PathVariable id: Long) = zootopiaService.getEnclosureById(id)
+
+    @GetMapping("/enclosures/{id}/animals")
+    fun getAnimalsByEnclosure(@PathVariable id: Long) = zootopiaService.getAnimalsByEnclosure(id)
+
+    @PostMapping("/animals")
+    fun createAnimal(@RequestBody animal: AnimalEntity): AnimalEntity = zootopiaService.newAnimal(animal)
+
+    @PostMapping("/enclosures")
+    fun createEnclosure(@RequestBody enclosure: EnclosureEntity): EnclosureEntity = zootopiaService.newEnclosure(enclosure)
+
+    @DeleteMapping("/animals/{id}")
+    fun deleteAnimal(@PathVariable id: Long) = zootopiaService.removeAnimal(id)
+
+    @DeleteMapping("/enclosures/{id}")
+    fun deleteEnclosure(@PathVariable id: Long) = zootopiaService.removeEnclosure(id)
+
+    @PutMapping("/animals/{id}")
+    fun updateAnimal(@PathVariable id: Long, @RequestBody animal: AnimalEntity) = zootopiaService.updateAnimal(id, animal)
+
+    @PutMapping("/enclosures/{id}")
+    fun updateEnclosure(@PathVariable id: Long, @RequestBody enclosure: EnclosureEntity) = zootopiaService.updateEnclosure(id, enclosure)
 }
--- src/main/kotlin/eu/udemx/zootopia/services/ZootopiaService.kt
+++ src/main/kotlin/eu/udemx/zootopia/services/ZootopiaService.kt
@@ -1,7 +1,59 @@
 package eu.udemx.zootopia.services
 
+import eu.udemx.zootopia.models.entities.AnimalEntity
+import eu.udemx.zootopia.models.entities.EnclosureEntity
+import eu.udemx.zootopia.models.repositories.AnimalRepository
+import eu.udemx.zootopia.models.repositories.EnclosureRepository
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
+import org.springframework.web.server.ResponseStatusException
 
 @Service
-class ZootopiaService {
+class ZootopiaService(
+    val animalRepository: AnimalRepository,
+    val enclosureRepository: EnclosureRepository
+) {
+    fun getAllAnimals(): List<AnimalEntity> = animalRepository.findAll()
+
+    fun getAnimalById(id: Long): AnimalEntity = animalRepository.findByIdOrNull(id) ?:
+        throw ResponseStatusException(HttpStatus.NOT_FOUND)
+
+    fun getAnimalsByEnclosure(id: Long): List<AnimalEntity> {
+        return if(enclosureRepository.existsById(id)) animalRepository.findAllByEnclosureId(id)
+        else throw ResponseStatusException(HttpStatus.NOT_FOUND)
+    }
+
+    fun newAnimal(animal: AnimalEntity): AnimalEntity = animalRepository.save(animal)
+
+    fun removeAnimal(id: Long) {
+        if (animalRepository.existsById(id)) animalRepository.deleteById(id)
+        else throw ResponseStatusException(HttpStatus.NOT_FOUND)
+    }
+
+    fun updateAnimal(id: Long, animal: AnimalEntity): AnimalEntity {
+        return if (animalRepository.existsById(id)) {
+            animal.id = id
+            animalRepository.save(animal)
+        } else throw ResponseStatusException(HttpStatus.NOT_FOUND)
+    }
+
+    fun getAllEnclosures(): List<EnclosureEntity> = enclosureRepository.findAll()
+
+    fun getEnclosureById(id: Long): EnclosureEntity = enclosureRepository.findByIdOrNull(id) ?:
+        throw ResponseStatusException(HttpStatus.NOT_FOUND)
+
+    fun newEnclosure(enclosure: EnclosureEntity): EnclosureEntity = enclosureRepository.save(enclosure)
+
+    fun removeEnclosure(id: Long) {
+        if(enclosureRepository.existsById(id)) enclosureRepository.deleteById(id)
+        else throw ResponseStatusException(HttpStatus.NOT_FOUND)
+    }
+
+    fun updateEnclosure(id: Long, enclosure: EnclosureEntity): EnclosureEntity {
+        return if(animalRepository.existsById(id)) {
+            enclosure.id = id
+            enclosureRepository.save(enclosure)
+        } else throw ResponseStatusException(HttpStatus.NOT_FOUND)
+    }
 }
