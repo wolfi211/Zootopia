992b348777bfe856a58950fd3087d52214c91861
Daniel Wolf <wolf.dani9@gmail.com>
2023-07-12 16:59:09 +0200

    added hibernate function to get all animals inside an enclosure

--- src/main/kotlin/eu/udemx/zootopia/models/repositories/AnimalRepository.kt
+++ src/main/kotlin/eu/udemx/zootopia/models/repositories/AnimalRepository.kt
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface AnimalRepository: JpaRepository<AnimalEntity, Long>
+interface AnimalRepository: JpaRepository<AnimalEntity, Long> {
+    fun findAllByEnclosureId(id: Long): List<AnimalEntity>
+}
