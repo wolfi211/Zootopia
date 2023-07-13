b620e5be0a84ac21d43dc10341a2ed72da9dbd7e
Wolf Dániel <wolf.dani9@gmail.com>
2023-07-13 14:39:09 +0200

    added missing semicolon at the end of enum values

--- src/main/kotlin/eu/udemx/zootopia/models/enums/AnimalType.kt
+++ src/main/kotlin/eu/udemx/zootopia/models/enums/AnimalType.kt
@@ -1,7 +1,7 @@
 package eu.udemx.zootopia.models.enums
 
 enum class AnimalType {
-    CARNIVORE, HERBIVORE, OMNIVORE
+    CARNIVORE, HERBIVORE, OMNIVORE;
 
     companion object {
         fun byNameIgnoreCaseOrNull(input: String): AnimalType? = values().firstOrNull { it.name.equals(input, true) }
