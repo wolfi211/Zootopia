e43fde5c3216e06639ef85a7c032e27318d66280
Wolf Dániel <wolf.dani9@gmail.com>
2023-07-13 19:27:24 +0200

    added enum type for all animals

--- src/main/kotlin/eu/udemx/zootopia/models/enums/AnimalType.kt
+++ src/main/kotlin/eu/udemx/zootopia/models/enums/AnimalType.kt
@@ -1,7 +1,7 @@
 package eu.udemx.zootopia.models.enums
 
 enum class AnimalType {
-    CARNIVORE, HERBIVORE, OMNIVORE;
+    CARNIVORE, HERBIVORE, OMNIVORE, ALL;
 
     companion object {
         fun byNameIgnoreCaseOrNull(input: String): AnimalType? = values().firstOrNull { it.name.equals(input, true) }
