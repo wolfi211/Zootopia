02f7a1230f46910d7580df9c75a49dfd5a279417
Wolf Dániel <wolf.dani9@gmail.com>
2023-07-14 12:35:27 +0200

    added jsonidentityinfo annotation to get all containing animals when queried

--- src/main/kotlin/eu/udemx/zootopia/models/entities/EnclosureEntity.kt
+++ src/main/kotlin/eu/udemx/zootopia/models/entities/EnclosureEntity.kt
@@ -1,12 +1,20 @@
 package eu.udemx.zootopia.models.entities
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo
+import com.fasterxml.jackson.annotation.ObjectIdGenerators
 import jakarta.persistence.*
 
 @Entity
 @Table(name = "enclosures")
+@JsonIdentityInfo(
+    generator = ObjectIdGenerators.PropertyGenerator::class,
+    property = "id")
 data class EnclosureEntity (
-    @Id var id: Long = 0,
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long = 0,
     var name: String = "",
-    var type: String = "",
-    @OneToMany @JoinColumn(name = "enclosure_id") val animalsContained: List<AnimalEntity> = emptyList()
+    @OneToMany
+    @JoinColumn(name = "enclosure_id")
+    val animalsContained: List<AnimalEntity> = emptyList()
 )
