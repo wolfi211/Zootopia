5075b8f3ba868c84be0fd06f4421843541e7b86c
Daniel Wolf <wolf.dani9@gmail.com>
2023-07-18 10:31:37 +0200

    added new method to get the enum from the subclass type

--- src/main/kotlin/eu/udemx/zootopia/models/enums/AnimalType.kt
+++ src/main/kotlin/eu/udemx/zootopia/models/enums/AnimalType.kt
@@ -1,9 +1,24 @@
 package eu.udemx.zootopia.models.enums
 
+import eu.udemx.zootopia.models.dtos.AnimalDto
+import eu.udemx.zootopia.models.dtos.CarnivoreDto
+import eu.udemx.zootopia.models.dtos.HerbivoreDto
+import eu.udemx.zootopia.models.dtos.OmnivoreDto
+
 enum class AnimalType {
     CARNIVORE, HERBIVORE, OMNIVORE, ALL;
 
     companion object {
-        fun byNameIgnoreCaseOrNull(input: String): AnimalType? = values().firstOrNull { it.name.equals(input, true) }
+        fun byNameIgnoreCaseOrNull(input: String): AnimalType? =
+            values().firstOrNull { it.name.equals(input, true) }
+
+        fun fromDtoClassOrNull(input: AnimalDto): AnimalType? {
+            return when(input::class) {
+                CarnivoreDto::class -> CARNIVORE
+                HerbivoreDto::class -> HERBIVORE
+                OmnivoreDto::class -> OMNIVORE
+                else -> null
+            }
+        }
     }
 }
