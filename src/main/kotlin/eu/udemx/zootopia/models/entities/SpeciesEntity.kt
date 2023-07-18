5b4be9c10087bd3805885e2cc6aef187b5c25a12
Daniel Wolf <wolf.dani9@gmail.com>
2023-07-18 10:28:38 +0200

    added new column common name, annotated entity lists/sets to jsonignore

--- src/main/kotlin/eu/udemx/zootopia/models/entities/SpeciesEntity.kt
+++ src/main/kotlin/eu/udemx/zootopia/models/entities/SpeciesEntity.kt
@@ -1,6 +1,7 @@
 package eu.udemx.zootopia.models.entities
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo
+import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.annotation.ObjectIdGenerators
 import jakarta.persistence.*
 
@@ -12,15 +13,19 @@ import jakarta.persistence.*
 data class SpeciesEntity(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    var id: Long = 0,
+    var id: Long? = 0,
     var name: String = "",
+    @Column(name = "common_name")
+    var commonName: String = "",
     @OneToMany
     @JoinColumn(name = "species_id")
+    @JsonIgnore
     val animals: List<AnimalEntity> = emptyList(),
     @ManyToMany
     @JoinTable(
         name = "prey_predators",
         joinColumns = [JoinColumn(name = "prey_id")],
         inverseJoinColumns = [JoinColumn(name = "predator_id")])
+    @JsonIgnore
     open val predators: Set<AnimalEntity> = emptySet(),
 )
