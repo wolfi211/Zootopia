cc45d4141e5cb23a964bbfffb52a680ad4cec1c3
Daniel Wolf <wolf.dani9@gmail.com>
2023-07-18 17:32:28 +0200

    added check before creation to verify that no other animal is preying on the new or vice versa in the given enclosure

--- src/main/kotlin/eu/udemx/zootopia/models/enums/AnimalType.kt
+++ src/main/kotlin/eu/udemx/zootopia/models/enums/AnimalType.kt
@@ -4,6 +4,10 @@ import eu.udemx.zootopia.models.dtos.AnimalDto
 import eu.udemx.zootopia.models.dtos.CarnivoreDto
 import eu.udemx.zootopia.models.dtos.HerbivoreDto
 import eu.udemx.zootopia.models.dtos.OmnivoreDto
+import eu.udemx.zootopia.models.entities.AnimalEntity
+import eu.udemx.zootopia.models.entities.CarnivoreEntity
+import eu.udemx.zootopia.models.entities.HerbivoreEntity
+import eu.udemx.zootopia.models.entities.OmnivoreEntity
 
 enum class AnimalType {
     CARNIVORE, HERBIVORE, OMNIVORE, ALL;
@@ -17,6 +21,15 @@ enum class AnimalType {
                 CarnivoreDto::class -> CARNIVORE
                 HerbivoreDto::class -> HERBIVORE
                 OmnivoreDto::class -> OMNIVORE
+                else -> null
+            }
+        }
+
+        fun fromEntityClassOrNull(input: AnimalEntity): AnimalType? {
+            return when(input::class) {
+                CarnivoreEntity::class -> CARNIVORE
+                HerbivoreEntity::class -> HERBIVORE
+                OmnivoreEntity::class -> OMNIVORE
                 else -> null
             }
         }
--- src/main/kotlin/eu/udemx/zootopia/services/AnimalService.kt
+++ src/main/kotlin/eu/udemx/zootopia/services/AnimalService.kt
@@ -1,6 +1,9 @@
 package eu.udemx.zootopia.services
 
 import eu.udemx.zootopia.models.entities.AnimalEntity
+import eu.udemx.zootopia.models.entities.CarnivoreEntity
+import eu.udemx.zootopia.models.entities.OmnivoreEntity
+import eu.udemx.zootopia.models.enums.AnimalType
 import eu.udemx.zootopia.models.repositories.AnimalRepository
 import eu.udemx.zootopia.models.repositories.EnclosureRepository
 import org.springframework.data.repository.findByIdOrNull
@@ -24,7 +27,10 @@ class AnimalService(
         else throw ResponseStatusException(HttpStatus.NOT_FOUND)
     }
 
-    fun newAnimal(animal: AnimalEntity): AnimalEntity = animalRepository.save(animal)
+    fun newAnimal(animal: AnimalEntity): AnimalEntity {
+        return if(isNotEligibleForEnclosure(animal)) animalRepository.save(animal)
+        else throw ResponseStatusException(HttpStatus.CONFLICT)
+    }
 
     fun removeAnimal(id: Long) {
         if (animalRepository.existsById(id)) animalRepository.deleteById(id)
@@ -36,5 +42,37 @@ class AnimalService(
             animal.id = id
             animalRepository.save(animal)
         } else throw ResponseStatusException(HttpStatus.NOT_FOUND)
+    }
+
+    fun isNotEligibleForEnclosure(animal: AnimalEntity): Boolean {
+        val enclosure = animal.enclosure ?: return true
+
+        for(containedAnimal in enclosure.animalsContained) {
+            if(AnimalType.fromEntityClassOrNull(containedAnimal) == AnimalType.CARNIVORE) {
+                for(prey in (containedAnimal as CarnivoreEntity).preys) {
+                    if(animal.species!!.id == prey.id) return false
+                }
+            } else if (AnimalType.fromEntityClassOrNull(containedAnimal) == AnimalType.OMNIVORE) {
+                for(prey in (containedAnimal as OmnivoreEntity).preys) {
+                    if(animal.species!!.id == prey.id) return false
+                }
+            }
+        }
+
+        if(AnimalType.fromEntityClassOrNull(animal) == AnimalType.CARNIVORE) {
+            for(prey in (animal as CarnivoreEntity).preys) {
+                for(containedAnimal in enclosure.animalsContained) {
+                    if(prey.id == containedAnimal.species!!.id) return false
+                }
+            }
+        } else if(AnimalType.fromEntityClassOrNull(animal) == AnimalType.OMNIVORE) {
+            for(prey in (animal as OmnivoreEntity).preys) {
+                for(containedAnimal in enclosure.animalsContained) {
+                    if(prey.id == containedAnimal.species!!.id) return false
+                }
+            }
+        }
+
+        return true
     }
 }
